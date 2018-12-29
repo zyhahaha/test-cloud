60e4aa1361409deeff310f6afc473b0572f2e15c
zhaoyang <18355403288@163.com>
2018-12-29 13:44:52 +0800

    mock

---
+++ assets/api.js
@@ -0,0 +1,29 @@
+let storeDetailData = {
+  data: {
+    notice: '店铺公告msg msg',
+    phone: '18312341234',
+    address: '上海市',
+    businessHours: '18:00 - 19:00',
+    intro: '商户信息tes tetst'
+  }
+}
+
+let personData = {
+  data: {
+    avatar: './assets/person_center/default_head.png',
+    username: '游客',
+    phone: '未绑定',
+    orderNum: '99',
+    serviceLog: '22',
+    collectNum: '2'
+  }
+}
+
+let personInfo = {
+  data: {
+    avatar: './assets/person_center/default_head.png',
+    username: '游客',
+    gender: 1,
+    phone: '未绑定',
+  }
+}
